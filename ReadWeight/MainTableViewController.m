c75c6b15e65450917b140d0cd2c808d5e7a0ac59
cchestnut91 <cchestnut91@gmail.com>
2014-10-02 19:32:07 -0400

    Release version, plus a few bug fixes that I don't believe were in the actual release version.

--- ReadWeight/MainTableViewController.m
+++ ReadWeight/MainTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainTableViewController.h"
+#import <Crashlytics/Crashlytics.h>
 #import "AppDelegate.h"
 #import "AddDrinkViewController.h"
 #import "BACLabelTableViewCell.h"
@@ -24,11 +25,18 @@
     NSString *bacFile;
     NSString *sessionFile;
     HKHealthStore *healthStore;
+    NSString *healthPermissionFile;
     BOOL showUber;
+    NSTimer *timer;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSURL *containerURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.com.calvinchestnut.drinktracker.sessionData"];
+    bacFile = [[containerURL URLByAppendingPathComponent:@"bac"] path];
+    
+    bac = 0.0;
+    [NSKeyedArchiver archiveRootObject:[NSNumber numberWithDouble:bac] toFile:bacFile];
     
     healthStore = [(AppDelegate *)[UIApplication sharedApplication].delegate healthStore];
     HKSampleQuery *weightQuery = [[HKSampleQuery alloc] initWithSampleType:[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMass] predicate:nil limit:1 sortDescriptors:@[[[NSSortDescriptor alloc] initWithKey:HKSampleSortIdentifierEndDate ascending:NO]] resultsHandler:^(HKSampleQuery *query, NSArray *results, NSError *error){
@@ -42,8 +50,6 @@
         }
     }];
     [healthStore executeQuery:weightQuery];
-    NSURL *containerURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.com.calvinchestnut.drinktracker.sessionData"];
-    bacFile = [[containerURL URLByAppendingPathComponent:@"bac"] path];
     if ([[NSFileManager defaultManager] fileExistsAtPath:bacFile]){
         bac = [(NSNumber *)[NSKeyedUnarchiver unarchiveObjectWithFile:bacFile] doubleValue];
     } else {
@@ -56,6 +62,8 @@
         showUber = false;
     }
     sessionFile = [[containerURL URLByAppendingPathComponent:@"drinkingSession"] path ];
+    healthPermissionFile = [[containerURL URLByAppendingPathComponent:@"askedPermission"] path];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addDrink:) name:@"newDrink" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(addDrinkFromURL:) name:@"addFromURL" object:nil];
     [[NSNotificationCenter defaultCenter] postNotificationName:@"checkLaunchURL" object:nil];
@@ -68,27 +76,38 @@
 }
 
 -(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
     if ([JNKeychain loadValueForKey:@"weight"] == nil || [JNKeychain loadValueForKey:@"sex"] == nil){
         [self performSegueWithIdentifier:@"getWeight" sender:self];
     } else {
-        [healthStore requestAuthorizationToShareTypes:[NSSet setWithObject:[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodAlcoholContent]] readTypes:[NSSet setWithObjects:[HKCharacteristicType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierBiologicalSex], [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMass], nil] completion:^(BOOL success, NSError *error){
-        }];
+        if (![[NSFileManager defaultManager] fileExistsAtPath:healthPermissionFile]){
+            [healthStore requestAuthorizationToShareTypes:[NSSet setWithObject:[HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodAlcoholContent]] readTypes:[NSSet setWithObjects:[HKCharacteristicType characteristicTypeForIdentifier:HKCharacteristicTypeIdentifierBiologicalSex], [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBodyMass], nil] completion:^(BOOL success, NSError *error){
+                [NSKeyedArchiver archiveRootObject:[NSNumber numberWithBool:YES] toFile:healthPermissionFile];
+            }];
+        }
     }
     
     if ([[NSFileManager defaultManager] fileExistsAtPath:sessionFile]){
-        NSDictionary *drinkingSession = [NSKeyedUnarchiver unarchiveObjectWithFile:sessionFile];
-        NSArray *drinks = [drinkingSession objectForKey:@"drinks"];
-        [self recalcBAC:drinks];
+        [self recalcBAC];
     } else {
         HKQuantityType *type = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodAlcoholContent];
         HKQuantitySample *bacSample = [HKQuantitySample quantitySampleWithType:type quantity:[HKQuantity quantityWithUnit:[HKUnit percentUnit] doubleValue:bac / 100] startDate:[NSDate date] endDate:[NSDate date]];
         [healthStore saveObject:bacSample withCompletion:nil];
     }
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:30 target:self selector:@selector(recalcBAC) userInfo:nil repeats:YES];
+    [timer fire];
     [self.bacLabel setText:[NSString stringWithFormat:@"%.3f", bac]];
 }
 
+-(void)viewWillDisappear:(BOOL)animated{
+    [timer invalidate];
+    
+    [super viewWillDisappear:animated];
+}
+
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    if (showUber){
+    if (showUber && !showUber){
         return 3;
     }
     return 2;
@@ -147,7 +166,7 @@
     NSMutableDictionary *drinkingSession;
     NSDictionary *userInfo = [notification userInfo];
     Drink *newDrink = [userInfo objectForKey:@"newDrink"];
-    if (bac == 0.0){
+    if (bac == 0.0 || bac != bac){
         drinkingSession = [[NSMutableDictionary alloc] init];
         [drinkingSession setObject:[newDrink time] forKey:@"startTime"];
         [drinkingSession setObject:[[NSArray alloc] init] forKey:@"drinks"];
@@ -158,7 +177,7 @@
     [drinks addObject:newDrink];
     [drinkingSession setObject:drinks forKey:@"drinks"];
     [NSKeyedArchiver archiveRootObject:drinkingSession toFile:sessionFile];
-    [self recalcBAC:drinks];
+    [self recalcBAC];
     [[UIApplication sharedApplication] cancelAllLocalNotifications];
     UILocalNotification *sober = [[UILocalNotification alloc] init];
     double secondsLeft = (bac / 0.015) * 60 * 60;
@@ -168,36 +187,62 @@
     [[UIApplication sharedApplication] scheduleLocalNotification:sober];
 }
 
--(void)recalcBAC:(NSArray *)drinks{
-    NSDictionary *drinkingSession = [NSKeyedUnarchiver unarchiveObjectWithFile:sessionFile];
-    double consumed = 0.0;
-    for (Drink *drink in drinks){
-        double add = [[drink multiplier] doubleValue];
-        consumed += add;
+-(void)recalcBAC{
+    if ([[NSFileManager defaultManager] fileExistsAtPath:sessionFile]){
+        NSDictionary *drinkingSession = [NSKeyedUnarchiver unarchiveObjectWithFile:sessionFile];
+        if (drinkingSession != nil){
+            NSArray *drinks = [drinkingSession objectForKey:@"drinks"];
+            double consumed = 0.0;
+            for (Drink *drink in drinks){
+                double add = [[drink multiplier] doubleValue];
+                consumed += add;
+            }
+            consumed = consumed * 0.806 * 1.2;
+            double genderStandard = [self genderStandard];
+            double kgweight =([[JNKeychain loadValueForKey:@"weight"] doubleValue] * 0.454);
+            double weightMod = genderStandard * kgweight;
+            double newBac = consumed / weightMod;
+            double hoursDrinking = [[NSDate date] timeIntervalSinceDate:[drinkingSession objectForKey:@"startTime"]] / 60.0 / 60.0;
+            double metabolized = [self metabolismConstant] * hoursDrinking;
+            bac = newBac - metabolized;
+            if (bac == INFINITY || bac == -INFINITY || bac != bac){
+                // Issue with some number
+                bac = 0.0;
+                [Crashlytics setFloatValue:[[JNKeychain loadValueForKey:@"weight"] floatValue] forKey:@"weight"];
+                [Crashlytics setFloatValue:genderStandard forKey:@"genderStandard"];
+                [Crashlytics setFloatValue:kgweight forKey:@"kgWeight"];
+                [Crashlytics setFloatValue:weightMod forKey:@"weightMod"];
+                [Crashlytics setFloatValue:newBac forKey:@"newBac"];
+                [Crashlytics setFloatValue:hoursDrinking forKey:@"hoursDrinking"];
+                [Crashlytics setFloatValue:metabolized forKey:@"metabolized"];
+                [Crashlytics setFloatValue:bac forKey:@"bac"];
+                UIAlertController *failed = [UIAlertController alertControllerWithTitle:@"Calculation Failed" message:@"There was an issue calculating your BAC. It was probably an issue saving your weight. Would you like to send a crash report? This will close the app." preferredStyle:UIAlertControllerStyleAlert];
+                [failed addAction:[UIAlertAction actionWithTitle:@"Send Report" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action){
+                    [[Crashlytics sharedInstance] crash];
+                }]];
+                [failed addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:nil]];
+                [self presentViewController:failed animated:YES completion:nil];
+            } else {
+                if (bac <= 0.0){
+                    [[NSFileManager defaultManager] removeItemAtPath:sessionFile error:nil];
+                    bac = 0.0;
+                }
+                HKQuantityType *type = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodAlcoholContent];
+                HKQuantitySample *bacSample = [HKQuantitySample quantitySampleWithType:type quantity:[HKQuantity quantityWithUnit:[HKUnit percentUnit] doubleValue:bac / 100] startDate:[NSDate date] endDate:[NSDate date]];
+                [healthStore saveObject:bacSample withCompletion:nil];
+                [NSKeyedArchiver archiveRootObject:[NSNumber numberWithDouble:bac] toFile:bacFile];
+                if (bac > 0.04){
+                    showUber = true;
+                } else {
+                    showUber = false;
+                }
+                [self.bacLabel setText:[NSString stringWithFormat:@"%.3f", bac]];
+                [self.tableView reloadData];
+            }
+        } else {
+            [[NSFileManager defaultManager] removeItemAtPath:sessionFile error:nil];
+        }
     }
-    consumed = consumed * 0.806 * 1.2;
-    double genderStandard = [self genderStandard];
-    double kgweight =([[JNKeychain loadValueForKey:@"weight"] doubleValue] * 0.454);
-    double weightMod = genderStandard * kgweight;
-    double newBac = consumed / weightMod;
-    double hoursDrinking = [[NSDate date] timeIntervalSinceDate:[drinkingSession objectForKey:@"startTime"]] / 60.0 / 60.0;
-    double metabolized = [self metabolismConstant] * hoursDrinking;
-    bac = newBac - metabolized;
-    if (bac <= 0.0){
-        [[NSFileManager defaultManager] removeItemAtPath:sessionFile error:nil];
-        bac = 0.0;
-    }
-    HKQuantityType *type = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodAlcoholContent];
-    HKQuantitySample *bacSample = [HKQuantitySample quantitySampleWithType:type quantity:[HKQuantity quantityWithUnit:[HKUnit percentUnit] doubleValue:bac / 100] startDate:[NSDate date] endDate:[NSDate date]];
-    [healthStore saveObject:bacSample withCompletion:nil];
-    [NSKeyedArchiver archiveRootObject:[NSNumber numberWithDouble:bac] toFile:bacFile];
-    if (bac > 0.04){
-        showUber = true;
-    } else {
-        showUber = false;
-    }
-    [self.bacLabel setText:[NSString stringWithFormat:@"%.3f", bac]];
-    [self.tableView reloadData];
 }
 
 -(double)metabolismConstant{
