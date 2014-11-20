6fcabeaa9f3d44e9c8d4e69be1546635fd287d8e
cchestnut91 <cchestnut91@gmail.com>
2014-11-19 22:03:22 -0500

    Added WatchKit Targets and changed version number

---
+++ ReadWeight/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  DrinkTracker
+//
+//  Created by Calvin Chestnut on 11/17/14.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController : UIViewController
+
+@end
--- ReadWeight/TransLoadingIndicator.m
+++ ReadWeight/TransLoadingIndicator.m
@@ -2,17 +2,22 @@
 //  TransLoadingIndicator.m
 //  RevCheckIn
 //
+//  This class creates and updates a loading indicator, visually similar to standard iOS indicators.
+//
 //  Created by Calvin Chestnut on 8/27/14.
-//  Copyright (c) 2014 Andrew Sowers. All rights reserved.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
 //
 
 #import "TransLoadingIndicator.h"
 
-@implementation TransLoadingIndicator {
-    
-}
+@implementation TransLoadingIndicator
 
+
+/*
+  Initialization method performs general set up with given frame
+*/
 -(id)initWithFrame:(CGRect)frame{
+    
     self = [super initWithFrame:frame];
     
     [self setUp];
@@ -20,14 +25,59 @@
     return self;
 }
 
--(id)initWithCoder:(NSCoder *)aDecoder{
-    self = [super initWithCoder:aDecoder];
+/*
+  Initializes UI ELements and Blur effects with default theme.
+  Hides unneeded elements
+*/
+-(void)setUp{
     
-    [self setUp];
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];;
+    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    [blurEffectView setFrame:self.bounds];
+    [self addSubview:blurEffectView];
     
-    return self;
+    // Vibrancy effect
+    UIVibrancyEffect *vibrancyEffect = [UIVibrancyEffect effectForBlurEffect:blurEffect];
+    UIVisualEffectView *vibrancyEffectView = [[UIVisualEffectView alloc] initWithEffect:vibrancyEffect];
+    [vibrancyEffectView setFrame:self.bounds];
+    
+    // Initializes Label and Indicator for default
+    
+    self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 108, 150, 42)];
+    [self.textLabel setText:@"Label"];
+    [self.textLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.textLabel setNumberOfLines:2];
+    
+    self.indicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(56, 56, 37, 37)];
+    [self.indicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    
+    // Initializes imageview and sets to hide
+    
+    self.image = [[UIImageView alloc] initWithFrame:self.indicator.frame];
+    [self.image setHidden:YES];
+    [self.image setContentMode:UIViewContentModeScaleAspectFit];
+    [self.image setClipsToBounds:YES];
+    [self.image setBackgroundColor:[UIColor clearColor]];
+    
+    // Add elements to the vibrancy view
+    [[vibrancyEffectView contentView] addSubview:self.textLabel];
+    [[vibrancyEffectView contentView] addSubview:self.indicator];
+    [[vibrancyEffectView contentView] addSubview:self.image];
+    
+    // Add the vibrancy view to the blur view
+    [[blurEffectView contentView] addSubview:vibrancyEffectView];
+    
+    // Rounded rectangles are better than regular rectangles
+    [self.layer setCornerRadius:25];
+    
+    // Don't bleed over
+    [self setClipsToBounds:YES];
+    
 }
 
+/*
+  Recreate the vibrancy effect with default settings with the light theme
+*/
 -(void)setLightStyle{
     UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];;
     UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
@@ -47,6 +97,9 @@
     [[blurEffectView contentView] addSubview:vibrancyEffectView];
 }
 
+/*
+ Recreate the vibrancy effect with default settings with the extra light theme
+ */
 -(void)setExtraLightStyle{
     UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];;
     UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
@@ -66,6 +119,9 @@
     [[blurEffectView contentView] addSubview:vibrancyEffectView];
 }
 
+/*
+ Recreate the vibrancy effect with default settings with the dark theme
+ */
 -(void)setDarkStyle{
     UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];;
     UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
@@ -85,47 +141,9 @@
     [[blurEffectView contentView] addSubview:vibrancyEffectView];
 }
 
--(void)setUp{
-    
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];;
-    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    [blurEffectView setFrame:self.bounds];
-    [self addSubview:blurEffectView];
-    
-    // Vibrancy effect
-    UIVibrancyEffect *vibrancyEffect = [UIVibrancyEffect effectForBlurEffect:blurEffect];
-    UIVisualEffectView *vibrancyEffectView = [[UIVisualEffectView alloc] initWithEffect:vibrancyEffect];
-    [vibrancyEffectView setFrame:self.bounds];
-    
-    //Shoudl send subview to back?
-    
-    self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 108, 150, 42)];
-    [self.textLabel setText:@"Label"];
-    [self.textLabel setTextAlignment:NSTextAlignmentCenter];
-    [self.textLabel setNumberOfLines:2];
-    
-    self.indicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(56, 56, 37, 37)];
-    [self.indicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhiteLarge];
-    
-    self.image = [[UIImageView alloc] initWithFrame:self.indicator.frame];
-    [self.image setHidden:YES];
-    [self.image setContentMode:UIViewContentModeScaleAspectFit];
-    [self.image setClipsToBounds:YES];
-    [self.image setBackgroundColor:[UIColor clearColor]];
-    
-    // Add label to the vibrancy view
-    [[vibrancyEffectView contentView] addSubview:self.textLabel];
-    [[vibrancyEffectView contentView] addSubview:self.indicator];
-    [[vibrancyEffectView contentView] addSubview:self.image];
-    
-    // Add the vibrancy view to the blur view
-    [[blurEffectView contentView] addSubview:vibrancyEffectView];
-    
-    [self.layer setCornerRadius:25];
-    [self setClipsToBounds:YES];
-    
-}
-
+/*
+  Show success message and check mark
+*/
 -(void)succeed{
     [self.textLabel setText:@"Success!"];
     [self.indicator setHidden:YES];
@@ -134,6 +152,9 @@
     [self fadeAway];
 }
 
+/*
+  Show Failed message and x
+*/
 -(void)fail{
     [self.textLabel setText:@"Failed"];
     [self.indicator setHidden:YES];
@@ -142,10 +163,17 @@
     [self fadeAway];
 }
 
+/*
+  Create delay to read the result message and fade the view to hidden
+*/
 -(void)fadeAway{
     [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(animate) userInfo:nil repeats:NO];
 }
 
+/*
+  Animate the view into a hidden state
+  Hide elements when finished
+*/
 -(void)animate{
     [self setHidden:NO];
     [UIView animateWithDuration:1 animations:^{
@@ -156,10 +184,16 @@
     }];
 }
 
+/*
+  Update indicator label text
+*/
 -(void)changeText:(NSString *)textIn{
     [self.textLabel setText:textIn];
 }
 
+/*
+  Unhides the view and starts loading indicator animation
+*/
 -(void)startAnimating{
     if (self.hidden){
         [self setHidden:NO];
@@ -169,21 +203,19 @@
     [self.indicator startAnimating];
 }
 
+/*
+  Stops loadingIndicator animation
+*/
 -(void)stopAnimating{
     [self.indicator stopAnimating];
 }
 
+/*
+  Immediately hides the loading view
+*/
 -(void)hide{
     [self stopAnimating];
     [self setHidden:YES];
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
