6a86c25c90b78dd00a7fe86efb173378067757ec
cchestnut91 <cchestnut91@gmail.com>
2014-11-23 12:41:28 -0500

    Initial WatchKitCommit

---
+++ ReadWeight WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  ReadWeight WatchKit Extension
+//
+//  Created by Calvin Chestnut on 11/23/14.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ ReadWeight WatchKit Extension/GlanceController.m
@@ -0,0 +1,43 @@
+//
+//  GlanceController.m
+//  ReadWeight WatchKit Extension
+//
+//  Created by Calvin Chestnut on 11/23/14.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
+//
+
+#import "GlanceController.h"
+
+
+@interface GlanceController()
+
+@end
+
+
+@implementation GlanceController
+
+- (instancetype)initWithContext:(id)context {
+    self = [super initWithContext:context];
+    if (self){
+        // Initialize variables here.
+        // Configure interface objects here.
+        NSLog(@"%@ initWithContext", self);
+        
+    }
+    return self;
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    NSLog(@"%@ will activate", self);
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    NSLog(@"%@ did deactivate", self);
+}
+
+@end
+
+
+
